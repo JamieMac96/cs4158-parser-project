736efd9d21b27c6e1dc0b75d0e9bed3c60056b63
JamieMac96 <jamiemac96@hotmail.com>
2018-10-29 15:36:51 +0000

    Lexical analysis now working

---
+++ mtokens.h
@@ -0,0 +1,14 @@
+#define BEGINING 1
+#define BODY 2
+#define END 3
+#define MOVE 4
+#define TO 5
+#define ADD 6
+#define INPUT 7
+#define PRINT 8
+#define IDENTIFIER 9
+#define INTEGER_SIZE 10
+#define INTEGER 11
+#define SEMICOLON 12
+#define STRING_LITERAL 13
+#define TERMINATOR 14
